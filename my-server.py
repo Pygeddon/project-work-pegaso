f8ca110f3d06497fd17ce51235f7de4e54b20644
Pygeddon <rfustinoni@hotmail.com>
2024-12-19 12:54:57 +0100

    commit

--- my-server.py
+++ my-server.py
@@ -29,25 +29,44 @@ class MyServer(BaseHTTPRequestHandler):
     def do_GET(self):
         parsed_path = urllib.parse.urlparse(self.path)
         query_params = urllib.parse.parse_qs(parsed_path.query)
-        
-        self.send_response(200)
-        self.send_header('Content-type', 'text/html')
-        self.send_header('Access-Control-Allow-Origin', '*')
-        self.end_headers()
-        
-        response = f"Received GET request with parameters: {json.dumps(query_params)}"
-        self.wfile.write(response.encode('utf-8'))
-        # Create a PDF with the received data
+        if parsed_path.path == "/booking":
+            self.send_response(200)
+            self.send_header('Content-type', 'text/html')
+            self.send_header('Access-Control-Allow-Origin', '*')
+            self.end_headers()
+            response = f"Received GET request with parameters: {query_params}"
+            self.wfile.write(response.encode('utf-8'))
+            # crea_PDF_Ticket(query_params)
+            
+        elif parsed_path.path == "/events":
+            self.send_response(200)
+            self.send_header('Content-type', 'application/json')
+            self.send_header('Access-Control-Allow-Origin', '*')
+            self.end_headers()
+            events = [
+                {"name": "Concerto di Natale", "description":"Un concerto di natale alla Royal Albert Hall", "image":"event1.jpg", "date": "2021-12-25"},
+                {"name": "Festa di Capodanno", "description":"Un concerto di natale alla Royal Albert Hall", "image":"event2.jpg", "date": "2021-12-31"},
+                {"name": "Festa della Befana", "description":"Un concerto di natale alla Royal Albert Hall", "image":"event3.jpg", "date": "2022-01-06"}
+            ]
+            self.wfile.write(json.dumps(events).encode('utf-8'))
+        else:
+            self.send_response(404)
+            self.send_header('Content-type', 'text/html')
+            self.end_headers()
+            self.wfile.write(b'404 - Not Found')
+
+
+def crea_PDF_Ticket(query_params):
+       # Create a PDF with the received data
         pdf = PDF()
         pdf.add_page()
         pdf.chapter_title('Ecco il Biglietto per l''evento prenotato!')
-        pdf.chapter_body(json.dumps(query_params, indent=4))
-
-        pdf_output_path = '/home/robsansterre/Documenti/MyCode/MyCode/project-work-pegaso/tickets/request_data.pdf'
+        ticket_data = query_params
+        pdf.chapter_body(f"Nome: {ticket_data['name'][0]}, Email: {ticket_data['email'][0]}, Evento: {ticket_data['event'][0]}, Data: {ticket_data['date'][0]}")
+        pdf_output_path = '/home/robsansterre/Documenti/MyCode/MyCode/project-work-pegaso/tickets/yourticket.pdf'
         pdf.output(pdf_output_path)
 
         print(f"\nPDF created at: {pdf_output_path}")
-
 
 def run(server_class=HTTPServer, handler_class=MyServer, port=4000):
     server_address = ('localhost', port)
--- script.js
+++ script.js
@@ -9,7 +9,7 @@ $("#book-button").on("click", function (event) {
   };
 
   $.ajax({
-    url: "http://localhost:4000",
+    url: "http://localhost:4000/tickets",
     type: "GET",
     data: formData,
     success: function (response) {
@@ -19,6 +19,53 @@ $("#book-button").on("click", function (event) {
     error: function (error) {
       console.error("Error submitting form:", error);
       alert("Error submitting form!");
+    },
+  });
+});
+$(document).ready(function () {
+  $.ajax({
+    url: "http://localhost:4000/events",
+    type: "GET",
+    success: function (response) {
+      console.log("Events data loaded successfully:", response);
+      // You can process the response and update the page as needed
+      var events = response;
+      for (let event of events) {
+        //$("#event").append(`<option value="${event.id}">${event.name}</option>`);
+        console.log(event.date);
+        $("#event-list").append(
+          `<div class="row"><div class="col-md-4">
+            <div class="card">
+              <img src="assets/` +
+            event.image +
+            `" class="card-img-top" alt="Event 1">
+              <div class="card-body">
+                <h5 class="card-title">` +
+            event.name +
+            `</h5>
+                <p class="card-text">` +
+            event.description +
+            `</p>
+                <label for="date1">Date disponibili:</label>
+                <select id="date1" class="form-control" defa>
+                  <option selected="selected">` +
+            event.date +
+            `</option>
+                  <option>2025-01-02</option>
+                  <option>2025-01-03</option>
+                  <option>2025-01-04</option>
+                  <option>2025-01-05</option>
+                </select>
+                <button class="btn btn-primary mt-3">Buy Ticket</button>
+              </div>
+            </div>
+          </div>
+          </div>`
+        );
+      }
+    },
+    error: function (error) {
+      console.error("Error loading events data:", error);
     },
   });
 });
